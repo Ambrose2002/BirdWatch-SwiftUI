5d68fbf20c89ff24668a65d9d99cfebd4dadb73f
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-04 21:38:54 -0500

    create birdInfoRow

--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        birdInfoRow
+    }
+    
+    private var birdInfoRow: some View {
+        HStack{
+            Text("6")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.trailing, 30)
+            
+            VStack(alignment: .leading){
+                Text("Antoinette")
+                    .fontWeight(.bold)
+                Text("New York")
+                    .fontWeight(.medium)
+                    .italic()
+            }
+            .font(.title2)
+            
+            Spacer()
+            
+            Image("antoinette")
+                .resizable()
+                .frame(width: 100, height: 100)
         }
-        .padding()
     }
 }
 
